619b6fb417e899fe33dc72323978447c5d4e69af
Mustafa Tanrıverdi <tanriverdi1907mustafa@gmail.com>
2024-07-03 18:06:00 +0300

    Dongu Pratikleri

---
+++ src/D3_javaDonguler/P2_TekSayilarinToplami.java
@@ -0,0 +1,46 @@
+package D3_javaDonguler;
+/*
+Java döngüler ile negatif bir değer girilene kadar kullanıcıdan
+girişleri kabul eden ve girilen değerlerden tek sayıları
+toplayıp ekrana basan programı yazıyoruz.
+
+Ödev
+
+Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri
+ kabul eden ve girilen değerlerden çift ve 4'ün katları olan sayıları
+  toplayıp ekrana basan programı yazıyoruz.
+
+ */
+
+import java.util.Scanner;
+
+public class P2_TekSayilarinToplami {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayi = 0, tekSayiToplami = 0, toplam = 0;
+        /*do {
+            System.out.println("Sayi giriniz \t\t Cikmak icin -1 girin :");
+            sayi = input.nextInt();
+            if (sayi % 2 != 0 && sayi > 0) {
+                System.out.println(sayi + " bir tek sayi");
+                tekSayiToplami += sayi;
+            }
+
+        } while (sayi >= 0);
+        System.out.println("Girilen tek sayilarin toplami = " + tekSayiToplami);
+
+         */
+
+        do {
+            System.out.println("Sayi giriniz \t\t Cikmak icin tek sayi girin :");
+            sayi = input.nextInt();
+            if (sayi % 4 == 0) {
+                toplam += sayi;
+                System.out.println(sayi + " sayisi 4'un katidir.");
+            }
+        } while (sayi % 2 == 0);
+
+        System.out.println("4'un kati olan sayilarin toplami = "+toplam);
+
+    }
+}
