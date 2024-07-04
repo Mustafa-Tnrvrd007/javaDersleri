49c82457a61d3e3f8b624040743e6593edaa272f
Mustafa Tanrıverdi <tanriverdi1907mustafa@gmail.com>
2024-07-04 16:41:08 +0300

    Donguler Final

---
+++ src/D3_javaDonguler/P15_TersUcgen.java
@@ -0,0 +1,51 @@
+package D3_javaDonguler;
+/*
+Java ile basamak sayısının kullanıcıdan alınan ve döngüler kullanılarak,
+yıldızlar(*) ile ekrana ters üçgen çizen programı yazın.
+
+Örnek
+Basamak Sayısı : 10
+
+*******************
+*****************
+***************
+*************
+***********
+*********
+*******
+*****
+***
+*
+
+ */
+
+import java.util.Scanner;
+
+public class P15_TersUcgen {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ucgen kac Basamakli olsun : ");
+        int basamakSayisi, boslukSayisi = 0, yildizSayisi = 1;
+        basamakSayisi = input.nextInt();
+
+        // yildiz sayisini hesaplayalim
+        for (int i = 0; i < (basamakSayisi - 1); i++) {
+            yildizSayisi += 2;
+        }
+
+        for (int i = 0; i < basamakSayisi; i++) { // dis dongu
+            for (int j = 0; j < boslukSayisi; j++) {
+                System.out.print(" ");
+            }
+            boslukSayisi++;
+            for (int j = 0; j < yildizSayisi; j++) {
+                System.out.print("*");
+            }
+            yildizSayisi -= 2;
+            System.out.println();
+
+        }
+
+    }
+}
